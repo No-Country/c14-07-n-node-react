270746acc56b5efcf813d6d317bb51f148341d29
Gonzalo Martinez <gonzalovam@hotmail.com>
2023-10-11 23:20:54 -0300

    Express config

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const route = require("./routes/index.js");
+const morgan = require("morgan");
+const cors = require("cors");
 
-
+////// Inicialización
 const app = express()
 
 
+////// Configuracion
 
 
+////// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
-// Routes
-app.use("/test", route)
+
+////// Routes
+app.use("/test", require("./routes/index.js"))
+
+
 
 module.exports = app
