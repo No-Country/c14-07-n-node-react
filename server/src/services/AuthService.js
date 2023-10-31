e4e4dd1ad382aa827a984d4b4ca7efe9b66cb114
Jair Eduardo <jair@facturatech.co>
2023-10-30 23:48:27 -0600

    Generate token, refactor signUp, and implemented login

--- server/src/services/AuthService.js
+++ server/src/services/AuthService.js
@@ -1,27 +1,45 @@
 const { badRequestError } = require('../utils/errors/HttpErrorFactory');
+
 const bcrypt = require("bcrypt");
 const User = require('../models/AuthModel');
+const { createAccessToken } = require('../utils/jwt');
 
 const signup = async (body) => {
   const hashPassword = await bcrypt.genSalt();
-  const password = await bcrypt.hash(body.password,hashPassword);
+  const password = await bcrypt.hash(body.password, hashPassword);
 
-  const createUser = new User({
-    first_name: body.first_name,
-    second_name: body.second_name,
-    surname: body.surname,
-    second_surname: body.second_surname,
-    email: body.email,
-    password,
-    identification_number: body.identification_number,
-    age: body.age,
-    phone: body.phone,
-    role: body.role,
-    status: body.status,
-    token: body.token,
-  });
   try {
-    return createUser.save();
+    const createUser = new User({
+      first_name: body.first_name,
+      second_name: body.second_name,
+      surname: body.surname,
+      second_surname: body.second_surname,
+      email: body.email,
+      password,
+      identification_number: body.identification_number,
+      age: body.age,
+      phone: body.phone,
+      role: body.role,
+      status: body.status,
+      token: body.token,
+    });
+
+    const user = await createUser.save();
+    const token = await createAccessToken({ id: user.id });
+
+    return {
+      id: user.id,
+      first_name: user.first_name,
+      second_name: user.second_name,
+      surname: user.surname,
+      second_surname: user.second_surname,
+      email: user.email,
+      identification_number: user.identification_number,
+      age: user.age,
+      phone: user.phone,
+      role: user.role,
+      token,
+    };
   } catch (err) {
     return badRequestError(err);
   }
