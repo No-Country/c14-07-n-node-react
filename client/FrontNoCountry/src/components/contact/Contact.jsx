eca03f3bc5b3b50513af9377c817642ef5aefd7b
IsmaelChampy <ismael.hernandez@magnotechnology.com>
2023-11-01 21:19:14 -0600

    created new components

---
+++ client/FrontNoCountry/src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>Hola yo soy el contacto</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ client/FrontNoCountry/src/components/loader/LoadingScreen .jsx
@@ -0,0 +1,13 @@
+import "../../assets/css/loader/loader.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className="divisLoading">
+      <div id="preloader">
+        <div id="loader"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
